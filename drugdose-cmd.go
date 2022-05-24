63ebf742b44f9fafa9177d858926b6b2eb54de07
psybits <qwertywarrz@gmail.com>
2022-05-24 19:45:29 +0200

    easier to look at flags in the code

--- drugdose-cmd.go
+++ drugdose-cmd.go
@@ -11,50 +11,162 @@ import (
 )
 
 var (
-	drugname = flag.String("drug", "none", "The name of the drug."+
-		"\nTry to be as accurate as possible!")
-	drugroute = flag.String("route", "none", "oral, smoked, sublingual, insufflation, inhalation,"+
-		"\nintravenous, etc.")
-	drugargdose = flag.Float64("dose", 0, "just a number, without any units such as ml around it")
-	drugunits   = flag.String("units", "none", "the units themselves: ml, L, mg etc.")
-	drugperc    = flag.Float64("perc", 0, "this is only used for alcohol currently, again just a number,"+
-		"\nno % around it")
-	setEndTime = flag.Bool("set-end-time", false, "set the end time on the last log")
-	setEndID   = flag.Int("set-end-id", 0, "set the end time on a log ID, use -get-all to get the ID")
-	forUser    = flag.String("user", "default", "log for a specific user, for example if you're looking"+
-		"\nafter a friend")
-	getLast = flag.Int("get-last", 0, "print the last N number of logs")
-	getAll  = flag.Bool("get-all", false, "print all logs")
-	apiName = flag.String("apiname", "default", "the name of the API that you want to initialise for"+
-		"\nsettings.toml and sources.toml")
-	apiUrl = flag.String("apiurl", "default", "the URL of the API that you want to initialise for"+
-		"\nsources.toml combined with -apiname")
-	recreateSettings = flag.Bool("recreate-settings", false, "recreate the settings.toml file")
-	recreateSources  = flag.Bool("recreate-sources", false, "recreate the sources.toml file")
-	cleanLogs        = flag.Bool("clean-logs", false, "cleans the logs for the specified user name,"+
-		"\noptionally using the -user option")
-	removeNew = flag.Int("clean-new-logs", 0, "cleans the N number of newest logs")
-	removeOld = flag.Int("clean-old-logs", 0, "cleans the N number of oldest logs")
-	removeID  = flag.Int("clean-id", 0, "removes a single ID from the logs, use -get-all to see the IDs")
-	dbDir     = flag.String("db-dir", "default", "full path of the DB directory, this will work only on"+
-		"\nthe initial run, you can change it later in the settings.toml,"+
-		"\ndon't forget to delete the old DB directory")
-	getDirs       = flag.Bool("get-dirs", false, "prints the settings and DB directories path")
-	localInfoDrug = flag.String("local-info-drug", "none", "print info about drug from local DB,"+
-		"\nfor example if you've forgotten routes and units")
-	dontLog    = flag.Bool("dont-log", false, "only fetch info about drug to local DB, but don't log anything")
-	removeDrug = flag.String("remove-info-drug", "none", "remove all entries of a single drug from the info DB")
-	cleanDB    = flag.Bool("clean-db", false, "remove all tables from the DB")
-	getTimes   = flag.Bool("get-times", false, "get the times till onset, comeup, etc."+
-		"\naccording to the current time")
-	getTimesID    = flag.Int("get-times-id", 0, "get the times for a specific log id")
-	stopOnCfgInit = flag.Bool("stop-on-config-init", false, "stops the program once the config"+
-		"\nfiles have been initialised")
-	stopOnDbInit = flag.Bool("stop-on-db-init", false, "stops the program once the DB file has"+
-		"\nbeen created and initialised, if it doesn't exists already")
-	verbose  = flag.Bool("verbose", false, "print extra information")
-	remember = flag.Bool("remember", false, "remember last dose config")
-	forget   = flag.Bool("forget", false, "forget the last set remember config")
+	drugname = flag.String(
+		"drug",
+		"none",
+		"The name of the drug.\nTry to be as accurate as possible!")
+
+	drugroute = flag.String(
+		"route",
+		"none",
+		"oral, smoked, sublingual, insufflation, inhalation,\nintravenous, etc.")
+
+	drugargdose = flag.Float64(
+		"dose",
+		0,
+		"just a number, without any units such as ml around it")
+
+	drugunits = flag.String(
+		"units",
+		"none",
+		"the units themselves: ml, L, mg etc.")
+
+	drugperc = flag.Float64(
+		"perc",
+		0,
+		"this is only used for alcohol currently, again just a number,\nno % around it")
+
+	setEndTime = flag.Bool(
+		"set-end-time",
+		false,
+		"set the end time on the last log")
+
+	setEndID = flag.Int(
+		"set-end-id",
+		0,
+		"set the end time on a log ID, use -get-all to get the ID")
+
+	forUser = flag.String(
+		"user",
+		"default",
+		"log for a specific user, for example if you're looking\nafter a friend")
+
+	getLast = flag.Int(
+		"get-last",
+		0,
+		"print the last N number of logs")
+
+	getAll = flag.Bool(
+		"get-all",
+		false,
+		"print all logs")
+
+	apiName = flag.String(
+		"apiname",
+		"default",
+		"the name of the API that you want to initialise for\nsettings.toml and sources.toml")
+
+	apiUrl = flag.String(
+		"apiurl",
+		"default",
+		"the URL of the API that you want to initialise for\nsources.toml combined with -apiname")
+
+	recreateSettings = flag.Bool(
+		"recreate-settings",
+		false,
+		"recreate the settings.toml file")
+
+	recreateSources = flag.Bool(
+		"recreate-sources",
+		false,
+		"recreate the sources.toml file")
+
+	cleanLogs = flag.Bool(
+		"clean-logs",
+		false,
+		"cleans the logs for the specified user name,\noptionally using the -user option")
+
+	removeNew = flag.Int(
+		"clean-new-logs",
+		0,
+		"cleans the N number of newest logs")
+
+	removeOld = flag.Int(
+		"clean-old-logs",
+		0,
+		"cleans the N number of oldest logs")
+
+	removeID = flag.Int(
+		"clean-id",
+		0,
+		"removes a single ID from the logs, use -get-all to see the IDs")
+
+	dbDir = flag.String(
+		"db-dir",
+		"default",
+		"full path of the DB directory, this will work only on"+
+			"\nthe initial run, you can change it later in the settings.toml,"+
+			"\ndon't forget to delete the old DB directory")
+
+	getDirs = flag.Bool(
+		"get-dirs",
+		false,
+		"prints the settings and DB directories path")
+
+	localInfoDrug = flag.String(
+		"local-info-drug",
+		"none",
+		"print info about drug from local DB,\nfor example if you've forgotten routes and units")
+
+	dontLog = flag.Bool(
+		"dont-log",
+		false,
+		"only fetch info about drug to local DB, but don't log anything")
+
+	removeDrug = flag.String(
+		"remove-info-drug",
+		"none",
+		"remove all entries of a single drug from the info DB")
+
+	cleanDB = flag.Bool(
+		"clean-db",
+		false,
+		"remove all tables from the DB")
+
+	getTimes = flag.Bool(
+		"get-times",
+		false,
+		"get the times till onset, comeup, etc.\naccording to the current time")
+
+	getTimesID = flag.Int(
+		"get-times-id",
+		0,
+		"get the times for a specific log id")
+
+	stopOnCfgInit = flag.Bool(
+		"stop-on-config-init",
+		false,
+		"stops the program once the config\nfiles have been initialised")
+
+	stopOnDbInit = flag.Bool(
+		"stop-on-db-init",
+		false,
+		"stops the program once the DB file has\nbeen created and initialised, if it doesn't exists already")
+
+	verbose = flag.Bool(
+		"verbose",
+		false,
+		"print extra information")
+
+	remember = flag.Bool(
+		"remember",
+		false,
+		"remember last dose config")
+
+	forget = flag.Bool(
+		"forget",
+		false,
+		"forget the last set remember config")
 )
 
 type rememberConfig struct {
